e169dbdf34fef558b827e74dfb36a8b60242b0c8
Adam Bliss <abliss@gmail.com>
2014-05-05 10:46:33 +0000

    WIP: tweaking world schema

--- js/world_rarr.js
+++ js/world_rarr.js
@@ -1,42 +1,48 @@
-(function(w) {
-    if (!exports.worlds) exports.worlds = {};
-    exports.worlds[w.name] = w;
-}({
-    name:"&rarr;"
-    depends:[],
-    axioms:[  // Deductions only.
+{
+    "name":"&rarr;"
+    "depends":[],
+    "axioms":[  // 0-hyp theorems only.
         // ax1
-        {Bone:{Stmt:[0,"T0.0",[0,"T0.1","T0.0"]],Hyps:[],Free:[]},
-         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {"Bone":{"Stmt":[0,"T0.0",[0,"T0.1","T0.0"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
         // ax2
-        {Bone:{Stmt:[0,[0,"T0.0",[0,"T0.1","T0.2"]],[0,[0,"T0.0","T0.1"],[0,"T0.0","T0.2"]]],Hyps:[],Free:[]},
-         Meat:{Terms:["&rarr;"],Kinds:["wff"]}}        
+        {"Bone":{"Stmt":[0,[0,"T0.0",[0,"T0.1","T0.2"]],[0,[0,"T0.0","T0.1"],[0,"T0.0","T0.2"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}}        
     ],
-    secrets:[  // May contain inferences. Not displayed to user.
+    "secrets":[  // May contain inferences. Not displayed to user.
         // ax-mp
-        {Bone:{Stmt:T0.0,Hyps:["T0.1",[0,"T0.1","T0.0"]],Free:[]},
-         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {"Bone":{"Stmt":T0.0,"Hyps":["T0.1",[0,"T0.1","T0.0"]],"Free":[]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
         // To establish bindings on &rarr;
         // imim1
-        {Bone:{Stmt:[0,[0,"T0.0","T0.1"],[0,[0,"T0.1","T0.2"],[0,"T0.0","T0.2"]]],Hyps:[],Free:[]},
-         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0","T0.1"],[0,[0,"T0.1","T0.2"],[0,"T0.0","T0.2"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
         // imim2
-        {Bone:{Stmt:[0,[0,"T0.0","T0.1"],[0,[0,"T0.2","T0.0"],[0,"T0.2","T0.1"]]],Hyps:[],Free:[]},
-         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0","T0.1"],[0,[0,"T0.2","T0.0"],[0,"T0.2","T0.1"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
     ],
-    buttons:[ // One-hypothesis inferences only.
+    "buttons":[ // One-hypothesis inferences only.
     ],
-    goals:[
-        "() () (rarr (rarr A B) (rarr (rarr C A) (rarr C B)))" ,
-        "() () (rarr (rarr A B) (rarr (rarr B C) (rarr A C)))" ,
-        "() () (rarr (rarr (rarr A B) C) (rarr B C))" ,
-        "() () (rarr (rarr A (rarr B C)) (rarr B (rarr A C)))" ,
-        "() () (rarr (rarr A B) (rarr A A))",
-        "() () (rarr A (rarr B B))",
-        "() () (rarr A A)",
-        "() () (rarr A (rarr (rarr A B) B))" ,
-        "() () (rarr (rarr (rarr A A) B) B)" ,
-        "() () (rarr (rarr A (rarr A B)) (rarr A B))",
-    ]
-]
-});
+    "goals":[
+        {"Bone":{"Stmt":[0,[0,"T0.0","T0.1"],[0,[0,"T0.2","T0.0"],[0,"T0.2","T0.1"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0","T0.1"],[0,[0,"T0.1","T0.2"],[0,"T0.0","T0.2"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,[0,"T0.0","T0.1"],"T0.2"],[0,"T0.1","T0.2"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0",[0,"T0.1","T0.2"]],[0,"T0.1",[0,"T0.0","T0.2"]]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0","T0.1"],[0,"T0.0","T0.0"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,"T0.0",[0,"T0.1","T0.1"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,"T0.0","T0.0"]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,"T0.0",[0,[0,"T0.0","T0.1"],"T0.1"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,[0,"T0.0","T0.0"],"T0.1"],"T0.1"]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+        {"Bone":{"Stmt":[0,[0,"T0.0",[0,"T0.0","T0.1"]],[0,"T0.0","T0.1"]]},
+         "Meat":{"Terms":["&rarr;"],"Kinds":["wff"]}},
+    }]
+}}
