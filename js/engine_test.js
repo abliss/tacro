29ac039f596977d50aba75ed927e54109bccfc20
Adam Bliss <abliss@gmail.com>
2014-05-09 12:31:48 +0000

    world -> land

--- js/engine_test.js
+++ js/engine_test.js
@@ -2,7 +2,9 @@ var Fs = require('fs');
 var Bencode = require('bencode');
 var Crypto = require('crypto');
 
-function getWorld(filename) {
+var lands = [];
+var state = {};
+function getLand(filename) {
     // Uses eval instead of json to allow comments and naked keys.
     // This is almost certainly a terrible idea.
     return eval("("+Fs.readFileSync(filename,'utf8')+")");
@@ -14,4 +16,19 @@ function fingerprint(obj) {
     return "bencode-sha1-" + hash.digest('hex');
 }
 
-console.log(fingerprint(getWorld("world_rarr.js")));
+function clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function startWork(fact) {
+    var work = clone(fact);
+    work.Bone.Hyps = [clone(work.Bone.Stmt)];
+    work.Bone.Tree = {};
+}
+lands.push(getLand("land_rarr.js"));
+state.land = lands[0];
+state.goal = 0;
+state.work = clone(words.goals[state.goal]);
+
+
+
