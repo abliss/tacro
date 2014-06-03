310148f622b8a962aece9e7dd499f5cbccf246c3
Adam Bliss <abliss@gmail.com>
2014-06-03 12:54:10 +0000

    remove symlink

--- js/land_rarr.js
+++ js/land_rarr.js
@@ -1,1 +1,47 @@
-../obj/bencode-sha1-6ee23f8fc55ff8c207520d524e8ce2edc1edb825
+{
+    name:"&rarr;",
+    depends:[],
+    axioms:[// 0-hyp theorems only.
+        // NB: instead of the usual Simp+Frege basis for positive propositional
+        // calculus, we use one of Hilbert's because it provides the necessary
+        // "binding theorems" for rarr over itself.
+        // ax1
+        {Bone:{Stmt:[0,"T0.0",[0,"T0.1","T0.0"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        // imim1
+        {Bone:{Stmt:[0,[0,"T0.0","T0.1"],[0,[0,"T0.1","T0.2"],[0,"T0.0","T0.2"]]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        // imim2
+        {Bone:{Stmt:[0,[0,"T0.0","T0.1"],[0,[0,"T0.2","T0.0"],[0,"T0.2","T0.1"]]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        // pm2.43
+        {Bone:{Stmt:[0,[0,"T0.0",[0, "T0.0", "T0.1"]],[0,"T0.0","T0.1"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+    ],
+    inferences:[  // Not displayed in the user's toolbox.
+        // ax-mp
+        {Bone:{Stmt:"T0.0",Hyps:["T0.1",[0,"T0.1","T0.0"]],Free:[]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+    ],
+    goals:[
+        // ax2
+        {Bone:{Stmt:[0,[0,"T0.0",[0,"T0.1","T0.2"]],[0,[0,"T0.0","T0.1"],[0,"T0.0","T0.2"]]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,[0,[0,"T0.0","T0.1"],"T0.2"],[0,"T0.1","T0.2"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,[0,"T0.0",[0,"T0.1","T0.2"]],[0,"T0.1",[0,"T0.0","T0.2"]]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,[0,"T0.0","T0.1"],[0,"T0.0","T0.0"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,"T0.0",[0,"T0.1","T0.1"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,"T0.0","T0.0"]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,"T0.0",[0,"T0.1",[0,[0,"T0.1","T0.2"],"T0.2"]]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,"T0.0",[0,[0,"T0.0","T0.1"],"T0.1"]]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+        {Bone:{Stmt:[0,[0,[0,"T0.0","T0.0"],"T0.1"],"T0.1"]},
+         Meat:{Terms:["&rarr;"],Kinds:["wff"]}},
+    ],
+}
