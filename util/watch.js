e43a6cdafc349bd94c01701cb1a04005ae49bdd0
Adam Bliss <abliss@gmail.com>
2014-11-06 13:40:14 -0800

    watch futzing

--- util/watch.js
+++ util/watch.js
@@ -6,27 +6,57 @@ var Storage = require('../script/storage.js');
 var Engine = require('../script/engine.js');
 var Fs = require('fs');
 
-var storage = new Storage();
+var storage = new Storage(Engine.fingerprint);
 var rootRef = storage.remote;
 var token = Fs.readFileSync("firebase_token.txt", "utf8").replace(/\s*$/,'');
 
-rootRef.auth(token, function(err) {
+function fbEscape(str) {
+    return encodeURIComponent(str).replace(/\./g,"%2E");
+}
+
+function getFbKey(fact) {
+    var core = JSON.stringify(fact.Core).
+        replace(/\[/g,'(').
+        replace(/\]/g,')');
+    var terms = fact.getCoreTermNames().map(fbEscape).join(",");
+    return core + ";" + terms + ";" + fbEscape(Engine.fingerprint(fact));
+}
+
+rootRef.authWithCustomToken(token, function(err, authData) {
     if (err) {
         console.log("Auth failed: " + err);
     } else {
-        console.log("Auth succeeeded!");
+        console.log("Auth succeeeded!" + authData);
     }
 });
+
 rootRef.child("incoming").child("fact").on('child_added', function(snap) {
     var value = snap.val();
+    if (!value) {
+        return;
+    }
     console.log("New fact: " + JSON.stringify(value));
     console.log("  at " + new Date(value.when) + ", " + (new Date() - value.when) + "ms ago");
     try {
         var fact = new Fact(JSON.parse(value.what));
-        fact.verify();
-        console.log("Verified!");
+        var key = getFbKey(fact);
+
+        if (fact.Tree.Cmd != 'stmt') {
+            fact.verify();
+            rootRef.child("checked").child("fact").child("byMark").child(key).set(value.what);
+        }
+        rootRef.child("checked").child("fact").child("pushed").child(snap.key()).set(value);
+        snap.ref().remove();
+        console.log("Verified! " + key);
     } catch (e) {
-        console.log("Didn't verify: " + e);
+        try {
+            var fact = new Fact(JSON.parse(value.what));
+            if (!fact.Tree.Cmd) {
+                snap.ref().remove();
+            }
+        } catch (e2) {
+            console.log("Didn't verify: " + e);
+        }
     }
     console.log("");
 })
