b7622809fb9edce405651752f8ff599bf9bb21e9
Adam Bliss <abliss@gmail.com>
2023-05-26 21:28:52 -0400

    Adds a version of dividesplus with soln
    Also sketches for perp and beta

---
+++ data/land_25_beta.js
@@ -0,0 +1,25 @@
+({
+    name:"&beta;",
+    depends:["&sbquo;"],
+    axioms:[    ],
+    goals:[{"Core":[[],
+               [0,
+                [1,0,[2,[3,[4,[3,[5,1,0]],2]],3]],
+                [6,2,3,1]
+               ],
+               [[1,0],[2,0],[3,0]]
+              ],
+       // don't forget: to verify, the terms should be numbered as if
+       // the conclusion needed no hypotheses
+       "Skin":{"Name":"df-min","TermNames":
+               ["&equals;","min","&vert;","&sect;","&times;","&sbquo;","&beta;"]},
+       "FreeMaps":[[],[[]],[],[],[],[],[]],
+           Tree:{Cmd:"defthm",Definiendum: 6}}
+,
+           {"Core":[[],
+                    [0,0,[0,1,[1,2,[2,[3,2,3],[4,[5,0,1,2],4]]]]],
+                    [[3,0,1,2],[4,0,1]]],
+            "Skin":{"TermNames":["&exists;","&forall;","&rarr;","&le;","&equals;","&beta;"]},
+            "FreeMaps":[[[]],[[]],[],[],[],[]]},
+    ]
+})
