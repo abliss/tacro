496d49aeadeb1ae3e84c46dbb9ce5b19d1599cd0
Adam Bliss <abliss@gmail.com>
2023-07-30 13:31:00 -0400

    Tidy up df-and, and provide new proofs for and thms.

--- data/land_02_and.js
+++ data/land_02_and.js
@@ -2,31 +2,20 @@
     name:"&and;",
     depends:["&not;"],  
     goals:[
-        // Future definitions using the biconditional can be automatic, but this
-        // definition must be manual. In addition, to keep id as the "universal
-        // first step", we add a layer of indirection so that the root of the
-        // expression is ->.
-        {Core:[[[0,
-                   [0,
-                    [1,[0,
-                      [0,
-                       [1,[0,0,[1,1]]],[1,[0,0,[1,1]]]],
-                      [1,[0,   [1,[0,0,[1,1]]],
-                       [1,[0,0,[1,1]]]]]]],
-                    2],
-                   2]],
-               [0,
-                [0,
-                 [1,[0,
+        {Core:[[[0,[0,
                      [0,
-                      [2,0,1],[1,[0,0,[1,1]]]],
-                     [1,[0,   [1,[0,0,[1,1]]],
-                         [2,0,1]]]]],
-                 2],
-                2],[]],
-         Skin:{TermNames:["&rarr;","&not;","&and;"]},
+                      [2,[0,0,[2,1]]],[2,[0,0,[2,1]]]],
+                     [2,[0,   [2,[0,0,[2,1]]],
+                         [2,[0,0,[2,1]]]]]],2]],
+               [0,[0,
+                     [0,
+                      [1,0,1],[2,[0,0,[2,1]]]],
+                     [2,[0,   [2,[0,0,[2,1]]],
+                         [1,0,1]]]],2]
+               ,[]],
+         Skin:{TermNames:["&rarr;","&and;","&not;"]},
          FreeMaps:[[],[],[]],
-         Tree:{Cmd:"defthm",Definiendum: 2,
+         Tree:{Cmd:"defthm",Definiendum: 1,
                Proof: ["Hyps.0"]}
         },
         {Core:[[],[0,[1,0,1],[2,[0,0,[2,1]]]],[]],
