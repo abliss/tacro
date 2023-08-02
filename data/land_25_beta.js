a4cd4fef94153cee6bf5d3042c34af9bcffa8dc2
Adam Bliss <abliss@gmail.com>
2023-08-01 23:12:38 -0400

    Add dummy-> to make the big beta thm usable.

--- data/land_25_beta.js
+++ data/land_25_beta.js
@@ -24,12 +24,7 @@
            Tree:{Cmd:"defthm",Definiendum: 4}}
         ,
         {"Core":[[],[0,[1,0,1],[2,[3,2,3,0],[3,2,3,1]]],[]],"Skin":{"TermNames":["&rarr;","&equals;","&harr;","&beta;"]},"FreeMaps":[[],[],[],[]]},
-           {"Core":[[],
-                    [0,0,[0,1,[1,2,[2,[3,2,3],
-                                    [4,[5,[6,0],1,2],
-                                     [1,4,[2,[7,4,2],5]]]]]]],
-                    [[3,0,1,2,4],[5,0,1,2]]],
-            "Skin":{"TermNames":["&exist;","&forall;","&rarr;","&le;","&harr;","&beta;","&sect;","&equals;"]},
-            "FreeMaps":[[[]],[[]],[],[],[],[],[],[]]},
+           {"Core":[[],[0,0,[1,1,[1,2,[2,3,[0,[3,3,4],[4,[5,[6,1],2,3],[2,5,[0,[7,5,3],6]]]]]]]],[[4,1,2,3,5],[6,1,2,3]]],"Skin":{"Name":"7GrYAB","HypNames":[],"DepNames":[],"VarNames":["V0","V1","V2","V3","V4","V5","V6"],"TermNames":["&rarr;","&exist;","&forall;","&le;","&harr;","&beta;","&sect;","&equals;"],"Delimiters":[]},"Tree":{"Cmd":"stmt","Deps":[],"Proof":[]},"FreeMaps":[[],[[]],[[]],[],[],[],[],[]]}
+,
     ]
 })
